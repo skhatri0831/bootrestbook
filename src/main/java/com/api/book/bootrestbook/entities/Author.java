b6677ce243761fe5e54c71fa64656393df9a442e
Samir <skhatri0831@gmail.com>
2021-09-01 17:18:03 -0500

    Initial commit
    This is the first commit on bootrestbook

---
+++ src/main/java/com/api/book/bootrestbook/entities/Author.java
@@ -0,0 +1,83 @@
+package com.api.book.bootrestbook.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int authorId;
+
+    @Column(name = "first_name")
+    private String firstName;
+    private String lastName;
+    private String language;
+
+    @OneToOne(mappedBy = "author")
+    @JsonBackReference
+    private Book book;
+
+    public Author(int authorId, String firstName, String lastName, String language) {
+        this.authorId = authorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.language = language;
+    }
+
+    public Author() {
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    @Override
+    public String toString() {
+        return "Author [authorId=" + authorId + ", firstName=" + firstName + ", language=" + language + ", lastName="
+                + lastName + "]";
+    }
+
+}
---
+++ src/main/java/com/api/book/bootrestbook/helper/FileUploadHelper.java
@@ -0,0 +1,48 @@
+package com.api.book.bootrestbook.helper;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileUploadHelper {
+
+    public final String UPLOAD_DIR = "C:\\Users\\skhat\\Desktop\\Spring Boot\\bootrestbook\\src\\main\\resources\\static\\image";
+
+    // public final String UPLOAD_DIR = new
+    // ClassPathResource("static/image/").getFile().getAbsolutePath();
+
+    // public FileUploadHelper() throws IOException {
+
+    // }
+
+    public boolean uploadFile(MultipartFile multipartFile) {
+        boolean f = false;
+
+        try {
+            // InputStream is = multipartFile.getInputStream();
+            // byte data[] = new byte[is.available()];
+            // is.read(data);
+            // // write
+            // FileOutputStream fos = new FileOutputStream(
+            // UPLOAD_DIR + File.separator + multipartFile.getOriginalFilename());
+            // fos.write(data);
+            // fos.flush();
+            // fos.close();
+            // f = true;
+
+            Files.copy(multipartFile.getInputStream(),
+                    Paths.get(UPLOAD_DIR + File.pathSeparator + multipartFile.getOriginalFilename()),
+                    StandardCopyOption.REPLACE_EXISTING);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return f;
+    }
+}
